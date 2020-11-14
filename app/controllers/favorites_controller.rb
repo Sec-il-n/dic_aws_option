47d46834c876582a02187f09f232f6a3618bb00e
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-11-14 09:57:57 +0900

    fixup! お気に入り機能追加

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+  def index
+    @favorites = current_user.favorite_feeds
+  end
+  def create
+    favorite = current_user.favorites.create(feed_id: params[:feed_id] )
+    redirect_to feeds_path, notice: "#{favorite.feed.user.name}さんの写真をお気に入りに追加しました。"
+  end
+  def destroy
+    @favorite = current_user.favorites.find_by(feed_id: params[:feed_id]).destroy
+
+    redirect_to feeds_path, notice: "#{@favorite.user.name}さんの写真をお気に入りから削除しました。"
+  end
+  # def show
+  #   @favorites = current_user.favorite_feeds
+  # end
+end
